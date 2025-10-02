873c4f0105070cfd362df10ec0699dfd2c72faed
ig:csiszi.06 <sinko.szilard@szbiszeged.hu>
2025-10-02 12:33:23 +0200

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from "./pages/Home";
 import './App.css'
-import Home from './pages/Home'
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Jegy from "./components/Jegy";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <Home />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jegy" element={<Jegy />} />
+      </Routes>
+    </BrowserRouter>
   )
-}
+} 
 
-export default App
+export default App;
