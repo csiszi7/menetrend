3d424919add36f4e53699fb9e9c33ca86e225b6d
markusregobenedek-blip <markus.rego.benedek@szbiszeged.hu>
2025-10-02 13:47:05 +0200

    change 13 47

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,10 +6,11 @@ const Navbar = () => {
   return (
     <div className="navbar-kontener">
               <Link to='/'>Home</Link>
-              <Link to='/'>Belföldi Utazás</Link>
-              <Link to='/'>Nemzetközi Utazás</Link>
-              <Link to='/'>Ügyfészolgálat</Link>
-              <Link to='/'>Késési biztosítás</Link>
+              <Link to='https://www.mavcsoport.hu/'>MÁV-CSOPORT</Link>
+              <Link to='https://www.mavcsoport.hu/mav-szemelyszallitas/belfoldi-utazas'>Belföldi Utazás</Link>
+              <Link to='https://www.mavcsoport.hu/mav-szemelyszallitas/nemzetkozi-utazas'>Nemzetközi Utazás</Link>
+              <Link to='https://www.mavcsoport.hu/ugyfelszolgalat/mav-ugyfelszolgalat'>Ügyfészolgálat</Link>
+              <Link to='https://jegy.mav.hu/bejelentkezes?compensationRequest=compensationRequest'>Késési biztosítás</Link>
     </div>
   );
 };
