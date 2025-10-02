1aaadb3c0e008a17758ca88f7ae2b301111afe5e
ig:csiszi.06 <sinko.szilard@szbiszeged.hu>
2025-10-02 12:47:54 +0200

    navbar viszonnylag keszen

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,6 +6,10 @@ const Navbar = () => {
   return (
     <div className="navbar-kontener">
               <Link to='/'>Home</Link>
+              <Link to='/'>Belföldi Utazás</Link>
+              <Link to='/'>Nemzetközi Utazás</Link>
+              <Link to='/'>Ügyfészolgálat</Link>
+              <Link to='/'>Késési biztosítás</Link>
     </div>
   );
 };
