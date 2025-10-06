1763c1db588c651fbfaa56e245b8fa4fc8f906bf
ig:csiszi.06 <sinko.szilard@szbiszeged.hu>
2025-10-06 13:18:44 +0200

    schedule opciok

--- backend/models/schedule.js
+++ backend/models/schedule.js
@@ -1,10 +1,53 @@
 const mongoose = require('mongoose');
 
 const scheduleSchema = new mongoose.Schema(
-   
     {
-        type: String,
-        required: true,
+        induloallomas: {
+            type: String,
+            required: true,
+        },
+            celallomas: {
+            type: String,
+            required: true,
+        },
+        idopontok: [
+            {
+                type: String,
+                required: true,
+            }
+        ],
+        allomasok: [
+            {
+                type: String,
+                required: true,
+            }
+        ],
+        kedvezmeny: {
+            type: String,
+                required: true
+        },
+        kortabla: {
+            type: String,
+                required: true
+        },
+        indulasideje: {
+            type: String,
+                required: true
+        
+        },
+        visszaideje: {
+            type: String,
+                required: true
+        },
+        klima: {
+            type: Boolean,
+            default: false
+        },
+        helyjegy: {
+            type: Boolean,
+            default: false
+        }
+
     },
     
     {
